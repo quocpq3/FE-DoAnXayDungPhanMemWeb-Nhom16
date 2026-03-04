a49a8e9ff909a68f82e68dce53027522f78328ce
quocpq3 <pquoc428@gmail.com>
2026-03-05 00:33:29 +0700

    fix error deploy

--- vite-project/src/layout/home/Footer.tsx
+++ vite-project/src/layout/home/Footer.tsx
@@ -1,6 +1,6 @@
-import { Layout } from "antd";
+// import { Layout } from "antd";
 
-const { Footer } = Layout;
+// const { Footer } = Layout;
 const FooterLayout: React.FC = () => {
   return (
     <>
